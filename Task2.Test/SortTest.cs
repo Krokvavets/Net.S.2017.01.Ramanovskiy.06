d76675595dac002699705d326a5c49972d3dae9d
Krokvavets <vladislav.ramanovskiy@gmail.com>
2017-03-27 19:50:39 +0200

    Task2

---
+++ Task2.Test/SortTest.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Task2.Test
+{
+    [TestFixture]
+    public class SortTest
+    {
+        [TestCase(new int[] { 3, 5, 6,}, new int[] { 1, 2, 3,}, new int[] { -1, 2, 63, 12, 3}, new int[] { -10, 14, 3, 54, 100,0 }, new int[] { 1, 2, 3,3}, "311-1-10")]
+        [TestCase(new int[] { 33, 1, 5,}, new int[] { 1, 1, 1,}, new int[] { -1, -2, -63, -12, 3}, new int[] { -10, 14, 3, 54, 100,0,0 }, new int[] { 1, 2, 3,3}, "1133-10-1")]
+        [TestCase(new int[] { -30, 14, 7,}, new int[] { 0, 12, -3, 27}, new int[] { -1}, new int[] { -110, 14, 43, 534, 100,0 }, new int[] { 1, 342, 3,3}, "1-10-30-110")]
+        [Test]
+        public void SortMinVlToMin_PositiveTest(int[] list0, int[] list1, int[] list2, int[] list3, int[] list4, string expectedResult)
+        {
+            int[][] array = new int[5][];
+            array[0] = list0;
+            array[1] = list1;
+            array[2] = list2;
+            array[3] = list3;
+            array[4] = list4;
+            array.SortMinVlToMin();
+            string str = array[0][0].ToString() + array[1][0].ToString() + array[2][0].ToString()+ array[3][0].ToString()+ array[4][0].ToString();
+            Assert.AreEqual(expectedResult, str);
+        }
+
+        [TestCase(new int[] { 3, 5, 6, }, new int[] { 1, 2, 3, }, new int[] { -1, 2, 63, 12, 3 }, new int[] { -10, 14, 3, 54, 100, 0 }, new int[] { 1, 2, 3, 3 }, "-10-1113")]
+        [TestCase(new int[] { 33, 1, 5, }, new int[] { 1, 1, 1, }, new int[] { -1, -2, -63, -12, 3 }, new int[] { -10, 14, 3, 54, 100, 0, 0 }, new int[] { 1, 2, 3, 3 }, "-1-103311")]
+        [TestCase(new int[] { -30, 14, 7, }, new int[] { 0, 12, -3, 27 }, new int[] { -1 }, new int[] { -110, 14, 43, 534, 100, 0 }, new int[] { 1, 342, 3, 3 }, "-110-300-11")]
+        [Test]
+        public void SortMinVlToMax_PositiveTest(int[] list0, int[] list1, int[] list2, int[] list3, int[] list4, string expectedResult)
+        {
+            int[][] array = new int[5][];
+            array[0] = list0;
+            array[1] = list1;
+            array[2] = list2;
+            array[3] = list3;
+            array[4] = list4;
+            array.SortMinVlToMax();
+            string str = array[0][0].ToString() + array[1][0].ToString() + array[2][0].ToString() + array[3][0].ToString() + array[4][0].ToString();
+            Assert.AreEqual(expectedResult, str);
+        }
+        [TestCase(new int[] { 3, 5, 6, }, new int[] { 1, 2, 3, }, new int[] { -1, 2, 63, 12, 3 }, new int[] { -10, 14, 3, 54, 100, 0 }, new int[] { 1, 2, 3, 3 }, "-10-1311")]
+        [TestCase(new int[] { 33, 1, 5, }, new int[] { 1, 1, 1, }, new int[] { -1, -2, -63, -12, 3 }, new int[] { -10, 14, 3, 54, 100, 0, 0 }, new int[] { 1, 2, 3, 3 }, "-10331-11")]
+        [TestCase(new int[] { -30, 14, 7, }, new int[] { 0, 12, -3, 27 }, new int[] { -1 }, new int[] { -110, 14, 43, 534, 100, 0 }, new int[] { 1, 342, 3, 3 }, "-11010-30-1")]
+        [Test]
+        public void SortMaxVlToMin_PositiveTest(int[] list0, int[] list1, int[] list2, int[] list3, int[] list4, string expectedResult)
+        {
+            int[][] array = new int[5][];
+            array[0] = list0;
+            array[1] = list1;
+            array[2] = list2;
+            array[3] = list3;
+            array[4] = list4;
+            array = array.SortMaxVlToMin();
+            string str = array[0][0].ToString() + array[1][0].ToString() + array[2][0].ToString() + array[3][0].ToString() + array[4][0].ToString();
+            Assert.AreEqual(expectedResult, str);
+        }
+        [TestCase(new int[] { 3, 5, 6, }, new int[] { 1, 2, 3, }, new int[] { -1, 2, 63, 12, 3 }, new int[] { -10, 14, 3, 54, 100, 0 }, new int[] { 1, 2, 3, 3 }, "113-1-10")]
+        [TestCase(new int[] { 33, 1, 5, }, new int[] { 1, 1, 1, }, new int[] { -1, -2, -63, -12, 3 }, new int[] { -10, 14, 3, 54, 100, 0, 0 }, new int[] { 1, 2, 3, 3 }, "1-1133-10")]
+        [TestCase(new int[] { -30, 14, 7, }, new int[] { 0, 12, -3, 27 }, new int[] { -1 }, new int[] { -110, 14, 43, 534, 100, 0 }, new int[] { 1, 342, 3, 3 }, "-1-3001-110")]
+        [Test]
+        public void SortMaxVlToMax_PositiveTest(int[] list0, int[] list1, int[] list2, int[] list3, int[] list4, string expectedResult)
+        {
+            int[][] array = new int[5][];
+            array[0] = list0;
+            array[1] = list1;
+            array[2] = list2;
+            array[3] = list3;
+            array[4] = list4;
+            array = array.SortMaxVlToMax();
+            string str = array[0][0].ToString() + array[1][0].ToString() + array[2][0].ToString() + array[3][0].ToString() + array[4][0].ToString();
+            Assert.AreEqual(expectedResult, str);
+        }
+        [TestCase(new int[] { 3, 5, 6, }, new int[] { 1, 2, 3, }, new int[] { -1, 2, 63, 12, 3 }, new int[] { -10, 14, 3, 54, 100, 0 }, new int[] { 1, 2, 3, 3 }, "-10-1311")]
+        [TestCase(new int[] { 33, 1, 5, }, new int[] { 1, 1, 1, }, new int[] { -1, -2, -63, -12, 3 }, new int[] { -10, 14, 3, 54, 100, 0, 0 }, new int[] { 1, 2, 3, 3 }, "-103311-1")]
+        [TestCase(new int[] { -30, 14, 7, }, new int[] { 0, 12, -3, 27 }, new int[] { -1 }, new int[] { -110, 14, 43, 534, 100, 0 }, new int[] { 1, 342, 3, 3 }, "-11010-1-30")]
+        [Test]
+        public void SortSumToMin_PositiveTest(int[] list0, int[] list1, int[] list2, int[] list3, int[] list4, string expectedResult)
+        {
+            int[][] array = new int[5][];
+            array[0] = list0;
+            array[1] = list1;
+            array[2] = list2;
+            array[3] = list3;
+            array[4] = list4;
+            array = array.SortSumToMin();
+            string str = array[0][0].ToString() + array[1][0].ToString() + array[2][0].ToString() + array[3][0].ToString() + array[4][0].ToString();
+            Assert.AreEqual(expectedResult, str);
+        }
+
+        [TestCase(new int[] { 3, 5, 6, }, new int[] { 1, 2, 3, }, new int[] { -1, 2, 63, 12, 3 }, new int[] { -10, 14, 3, 54, 100, 0 }, new int[] { 1, 2, 3, 3 }, "113-1-10")]
+        [TestCase(new int[] { 33, 1, 5, }, new int[] { 1, 1, 1, }, new int[] { -1, -2, -63, -12, 3 }, new int[] { -10, 14, 3, 54, 100, 0, 0 }, new int[] { 1, 2, 3, 3 }, "-11133-10")]
+        [TestCase(new int[] { -30, 14, 7, }, new int[] { 0, 12, -3, 27 }, new int[] { -1 }, new int[] { -110, 14, 43, 534, 100, 0 }, new int[] { 1, 342, 3, 3 }, "-30-101-110")]
+        [Test]
+        public void SortSumToMax_PositiveTest(int[] list0, int[] list1, int[] list2, int[] list3, int[] list4, string expectedResult)
+        {
+            int[][] array = new int[5][];
+            array[0] = list0;
+            array[1] = list1;
+            array[2] = list2;
+            array[3] = list3;
+            array[4] = list4;
+            array = array.SortSumToMax();
+            string str = array[0][0].ToString() + array[1][0].ToString() + array[2][0].ToString() + array[3][0].ToString() + array[4][0].ToString();
+            Assert.AreEqual(expectedResult, str);
+        }
+
+    }
+}
---
+++ Task2/Sort.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    struct Date
+    {
+        public int Numer { get; internal set; }
+        public int Value { get; internal set; }
+    }
+
+    public static class Sort
+    {
+        ///<summary>
+        /// The method is Algorithm Bubble Sort
+        ///</summary>
+        /// <param name="array">Input array</param>
+        ///<returns>sorted array</returns>
+        static private Date[] BubbleSort(Date[] array)
+        {
+            Date temp;
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[i].Value > array[j].Value)
+                    {
+                        temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;
+                    }
+                }
+            }
+            return array;
+        }
+        ///<summary>
+        /// The method is Algorithm Bubble Sort for matrix from largest to the smallest min. value in matrix
+        ///</summary>
+        /// <param name="array">Input matrix</param>
+        ///<returns>sorted array</returns>
+        public static int[][] SortMinVlToMin(this int[][] array)
+        {
+            int[][] sortedArray = SortMinVlToMax(array);
+            Array.Reverse(sortedArray);
+            return sortedArray;
+        }
+
+        ///<summary>
+        /// The method is Algorithm Bubble Sort for matrix from the smallest to the largest min. value in matrix
+        ///</summary>From the smallest to the largest
+        /// <param name="array">Input matrix</param>
+        ///<returns>sorted array</returns>
+
+        public static int[][] SortMinVlToMax(this int[][] array)
+        {
+            Date[] arrayForSort = new Date[array.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                arrayForSort[i].Value = array[i].Min();
+                arrayForSort[i].Numer = i;
+            }
+            return Enum(array, BubbleSort(arrayForSort));
+        }
+        ///<summary>
+        /// The method is Algorithm Bubble Sort for matrix from largest to the smallest max. value in matrix
+        ///</summary>
+        /// <param name="array">Input matrix</param>
+        ///<returns>sorted array</returns>
+
+        public static int[][] SortMaxVlToMin(this int[][] array)
+        {
+            int[][] sortedArray = SortMaxVlToMax(array);
+            Array.Reverse(sortedArray);
+            return sortedArray;
+        }
+        ///<summary>
+        /// The method is Algorithm Bubble Sort for matrix from the smallest to the largest max. value in matrix
+        ///</summary>From the smallest to the largest
+        /// <param name="array">Input matrix</param>
+        ///<returns>sorted array</returns>
+        public static int[][] SortMaxVlToMax(this int[][] array)
+        {
+            Date[] arrayForSort = new Date[array.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                arrayForSort[i].Value = array[i].Max();
+                arrayForSort[i].Numer = i;
+            }
+            return Enum(array, BubbleSort(arrayForSort));
+        }
+        ///<summary>
+        /// The method is Algorithm Bubble Sort for matrix from largest to the smallest string amount of matrix
+        ///</summary>
+        /// <param name="array">Input matrix</param>
+        ///<returns>sorted array</returns>
+        public static int[][] SortSumToMin(this int[][] array)
+        {
+            int[][] sortedArray = SortSumToMax(array);
+            Array.Reverse(sortedArray);
+            return sortedArray;
+        }
+        ///<summary>
+        /// The method is Algorithm Bubble Sort for matrix from largest to the smallest string amount of matrix
+        ///</summary>
+        /// <param name="array">Input matrix</param>
+        ///<returns>sorted array</returns>
+        public static int[][] SortSumToMax(int[][] array)
+        {
+            Date[] arrayForSort = new Date[array.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                arrayForSort[i].Value = array[i].Sum();
+                arrayForSort[i].Numer = i;
+            }
+            return Enum(array, BubbleSort(arrayForSort));
+        }
+
+        private static int[][] Enum(int[][] inputArray, Date[] sortedArray)
+        {
+            int[][] outputArray = new int[inputArray.Length][];
+            for (int i = 0; i < inputArray.Length; i++)
+                outputArray[i] = inputArray[sortedArray[i].Numer];
+            return outputArray;
+        }
+    }
+}
